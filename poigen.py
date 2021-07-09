dce41e3a9cac074568969a633aa004439138944d
widberg <jaredwidberg@gmail.com>
2021-07-08 22:37:01 -0400

    poi

--- poigen.py
+++ poigen.py
@@ -11,16 +11,21 @@ with open('docs/geo/poi.json', 'w') as output:
     with open(FUEL_DIR + 'GameTsc/Story/hubinfos.tsc', 'r') as input:
         pass
     with open(FUEL_DIR + 'GameTsc/Story/poi.tsc', 'r') as input:
-        pass
+        for line in input:
+            if line.startswith('AddTypePointOfInterest '):
+                coords = line[23:].split(' ')
+                features.append(Feature(geometry=Point((normalize(float(coords[3]), 65535.0, -65537.0), normalize(float(coords[1]), 65535.0, -65537.0)))))
     with open(FUEL_DIR + 'GameTsc/Story/miss_official.tsc', 'r') as input:
-        for line in input:
-            if line.startswith('EMD_SetStartPos '):
-                coords = line[16:].split(' ')
-                features.append(Feature(geometry=Point((normalize(float(coords[2]), 65535.0, -65537.0), normalize(float(coords[0]), 65535.0, -65537.0)))))
+        pass
+        # for line in input:
+        #     if line.startswith('EMD_SetStartPos '):
+        #         coords = line[16:].split(' ')
+        #         features.append(Feature(geometry=Point((normalize(float(coords[2]), 65535.0, -65537.0), normalize(float(coords[0]), 65535.0, -65537.0)))))
     with open(FUEL_DIR + 'GameTsc/Story/missChallenge.tsc', 'r') as input:
-        for line in input:
-            if line.startswith('EMD_SetStartPos '):
-                coords = line[16:].split(' ')
-                features.append(Feature(geometry=Point((normalize(float(coords[2]), 65535.0, -65537.0), normalize(float(coords[0]), 65535.0, -65537.0)))))
+        pass
+        # for line in input:
+        #     if line.startswith('EMD_SetStartPos '):
+        #         coords = line[16:].split(' ')
+        #         features.append(Feature(geometry=Point((normalize(float(coords[2]), 65535.0, -65537.0), normalize(float(coords[0]), 65535.0, -65537.0)))))
     data = geojson.dumps(FeatureCollection(features))
     output.write(data)
