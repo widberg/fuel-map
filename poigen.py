5755299bd4ba2680c54e67b90eeb34eac8f55abc
widberg <jaredwidberg@gmail.com>
2021-07-08 23:24:36 -0400

    poi

--- poigen.py
+++ poigen.py
@@ -8,8 +8,8 @@ max_x = 65535.0
 min_y = -65537.0
 max_y = 65535.0
 
-scale_x = 65530 / ((max_x - min_x) / 2 + 4096 + 320)
-scale_y = 65530 / ((max_y - min_y) / 2 + 4096 + 320)
+scale_x = 65530 / ((max_x - min_x) / 2 + 4096 + 350)
+scale_y = 65530 / ((max_y - min_y) / 2 + 4096 + 350)
 
 def normalize(val, max, min):
     return 0.5 - (val - min) / (max - min)
